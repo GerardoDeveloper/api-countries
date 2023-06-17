09ca750b277ee47465e1e6cad166129980d76fa3
Gerardo developer <gerardo.software.developer@gmail.com>
2023-06-17 15:11:39 -0300

    Configuraciones básicas e iniciales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('API')
+        .setDescription('Api Countries')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document, {
+        swaggerOptions: {
+            filter: true,
+            showRequestDuration: true,
+        },
+    });
+
+    await app.listen(3000);
 }
+
 bootstrap();
