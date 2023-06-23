6b05969affdda0d1508a115536743f6d6916f946
Gerardo developer <gerardo.software.developer@gmail.com>
2023-06-22 21:00:11 -0300

    Eliminación de métodos inecesarios

--- src/countries/controllers/countries.controller.ts
+++ src/countries/controllers/countries.controller.ts
@@ -24,22 +24,4 @@ export class CountriesController {
     findAll() {
         return this.countriesService.findAll();
     }
-
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.countriesService.findOne(+id);
-    }
-
-    @Patch(':id')
-    update(
-        @Param('id') id: string,
-        @Body() updateCountryDto: UpdateCountryDto,
-    ) {
-        return this.countriesService.update(+id, updateCountryDto);
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.countriesService.remove(+id);
-    }
 }
