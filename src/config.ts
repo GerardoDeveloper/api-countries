f058b6c08a925a1a5284cc25a02b391d4e030187
Gerardo developer <gerardo.software.developer@gmail.com>
2023-06-24 14:52:04 -0300

    Se elimino comentario

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
-/**
- * Archivo de configuración de los distintos servicios.
- * Esto permite tener tantos servicios dentro de la app como se desee.
- */
 export default registerAs('config', () => {
     return {
         soapCountries: {
